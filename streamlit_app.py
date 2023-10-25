9c8d311913eaf51913e79369ed5bcfc8ea2bc666
sarirak2003 <62616906+sarirak2003@users.noreply.github.com>
2023-10-24 21:54:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
 streamlit.title('My parents first Healty Diner')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Omega 3 and Blueberry Oatmeal')
+
+streamlit.text('Kale, Spinach and Rocket Smoothi')
+
+streamlit.text('Hard-Boiled Free-Ranged Eggs')
