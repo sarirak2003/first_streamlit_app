3a006227d81e092cd6e6710e60decc70e6b9d6e8
sarirak2003 <62616906+sarirak2003@users.noreply.github.com>
2023-10-26 14:19:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,4 +31,4 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
